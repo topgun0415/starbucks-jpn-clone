d3b46fa5d07acae0f5847997ac8a0598f26e1b6b
philipuuu <topgun0415@naver.com>
2022-03-29 01:52:17 +0900

    🎉 Javascript create & start

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 /** @format */
+
+const $toggleMenu = document.getElementById('toggle-btn');
+const $menuNav = document.querySelector('.menu-nav');
+const $fixedBox = document.querySelector('.fixed-box');
+
+function clickEventHandler(e) {
+  console.log(e.target);
+}
+
+$toggleMenu.addEventListener('click', clickEventHandler);
+$menuNav.addEventListener('click', clickEventHandler);
+$fixedBox.addEventListener('click', clickEventHandler);
