0d72a65a8c4b67afd1dc0947519e22d6da225888
philipuuu <topgun0415@naver.com>
2022-04-03 02:43:03 +0900

    ✨Feat : sub-navbar layout logic add

--- app.js
+++ app.js
@@ -1,13 +1,69 @@
 /** @format */
 
-const $toggleMenu = document.getElementById('toggle-btn');
+// Header
 const $menuNav = document.querySelector('.menu-nav');
-const $fixedBox = document.querySelector('.fixed-box');
+const $subMenu = document.querySelectorAll('.sub-menu-wrapper');
+const $cafe_wrapper = document.querySelector('.cafe-wrapper');
+const $cafe_goods_wrapper = document.querySelector('.coffee-goods-wrapper');
+const $company_wrapper = document.querySelector('.company-wrapper');
+const $service_wrapper = document.querySelector('.service-wrapper');
+const $store_wrapper = document.querySelector('.store-wrapper');
 
-function clickEventHandler(e) {
-  console.log(e.target);
+let preSubMenu;
+
+// Section
+const $fixedBox = document.querySelector('.fixed-box');
+const $fixedBox_delBtn = document.querySelector('.del-btn');
+
+function mouseOverHandler(e) {
+  if (e.target.textContent === 'Cafe') {
+    $subMenu[0].style.display = 'block';
+    subMenuChange(0);
+  } else if (e.target.textContent === 'Coffee&Goods') {
+    $subMenu[1].style.display = 'block';
+    subMenuChange(1);
+  } else if (e.target.textContent === 'Company') {
+    $subMenu[2].style.display = 'block';
+    subMenuChange(2);
+  } else if (e.target.textContent === 'Service') {
+    $subMenu[3].style.display = 'block';
+    subMenuChange(3);
+  } else if (e.target.textContent === 'Store') {
+    $subMenu[4].style.display = 'block';
+    subMenuChange(4);
+  }
+
+  function subMenuChange(num) {
+    if (!preSubMenu) {
+      preSubMenu = $subMenu[num];
+    } else {
+      preSubMenu.style.display = 'none';
+      preSubMenu = $subMenu[num];
+    }
+  }
 }
 
-$toggleMenu.addEventListener('click', clickEventHandler);
-$menuNav.addEventListener('click', clickEventHandler);
-$fixedBox.addEventListener('click', clickEventHandler);
+function clickDeleteHandler(e) {
+  e.target.parentNode.style.display = 'none';
+}
+
+// Header Event
+$menuNav.addEventListener('mouseover', mouseOverHandler);
+$subMenu[0].addEventListener('mouseleave', (e) => {
+  $subMenu[0].style.display = 'none';
+});
+$subMenu[1].addEventListener('mouseleave', (e) => {
+  $subMenu[1].style.display = 'none';
+});
+$subMenu[2].addEventListener('mouseleave', (e) => {
+  $subMenu[2].style.display = 'none';
+});
+$subMenu[3].addEventListener('mouseleave', (e) => {
+  $subMenu[3].style.display = 'none';
+});
+$subMenu[4].addEventListener('mouseleave', (e) => {
+  $subMenu[4].style.display = 'none';
+});
+
+// Section Event
+$fixedBox_delBtn.addEventListener('click', clickDeleteHandler);
